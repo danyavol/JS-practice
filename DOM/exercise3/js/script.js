79aa85a0c7e030df15e27a8354002bba427fb103
danyavol <48417874+danyavol@users.noreply.github.com>
2019-06-27 16:12:03 +0300

    exercise 3. Added SHIFT function. Finished CTRL.

--- DOM/exercise3/js/script.js
+++ DOM/exercise3/js/script.js
@@ -1,25 +1,29 @@
 'use strict'
 window.onload = function () {
 
-  var arrayD = [];
+  var arrayD = []; // массив содержащий координаты элементов-исключений(диагональных элементов)
   var s = 0, s2 = 0;
 
-  for (var i = 0; i < table.rows.length; i++) {
-    for (var j = 0; j < table.rows[i].cells.length; j++) {
+  for (var i = 0; i < table.rows.length; i++) { // i - по вертикали
+    for (var j = 0; j < table.rows[i].cells.length; j++) { // j - по горизонтали
       table.rows[i].cells[j].innerText = randomInteger(1,10);
-      if (i == j) {
+      if (i == j) { // главная диагональ
         arrayD.push([i,j]);
         table.rows[i].cells[j].style.background = "red";
 
         s += +table.rows[i].cells[j].innerText;
         sumM.innerText = `Сумма главной диагонали: ${s}`;
       }
-      if (j == table.rows[i].cells.length - 1 - i) {
+      if (j == table.rows[i].cells.length - 1 - i) { // побочная диагональ
         arrayD.push([i,j]);
         table.rows[i].cells[j].style.background = "green";
 
         s2 += +table.rows[i].cells[j].innerText;
         sumP.innerText = `Сумма побочной диагонали: ${s2}`;
+      }
+      if ((i == 0) || (i == table.rows.length-1) || (j == 0) || (j == table.rows[0].cells.length-1)) {
+        arrayD.push([i,j]);
+        table.rows[i].cells[j].style.background = "skyblue";
       }
     }
   }
@@ -39,17 +43,24 @@ window.onload = function () {
 
 
       var summa = 0;
-
+      var prevElem = [0,0];
 
       table.rows[i].cells[j].onclick = function () {
-        var cell = [i,j];
+
+        // Нахождение координат элемента this и запись их в cell[i,j]
+        for (var i = 0; i < table.rows.length; i++) {
+          for (var j = 0; j < table.rows[i].cells.length; j++) {
+            if (this == table.rows[i].cells[j]) var cell = [i,j];
+          }
+        }
+        // Проверка на то, является ли this элементом-исключением
         var flag = true;
         for (var i = 0; i < arrayD.length; i++) {
-          if ((cell[0] == arrayD[i][0]) && (cell[1] == arrayD[i][1])) {
+          if ((arrayD[i][0] == cell[0]) && (arrayD[i][1] == cell[1])) {
             flag = false;
           }
         }
-        console.log(flag);
+
 
         if (flag) {
           if (event.ctrlKey) {
@@ -63,7 +74,34 @@ window.onload = function () {
               summa -= +this.innerText;
               sum.innerText = `Сумма: ${summa}`;
             }
+            prevElem = [cell[0],cell[1]];
 
+          } else if (event.shiftKey) {
+            event.preventDefault();
+            var flag1 = true;
+            for (var i = prevElem[0]; i < cell[0] + 1; i++) {
+              for (var j = 0; j < table.rows[0].cells.length; j++) {
+
+                if (flag1) {
+                  j = prevElem[1]+1;
+                  flag1 = false;
+                }
+                if (j == cell[1]+1 && i == cell[0]) break;
+
+                var flag2 = true;
+                for (var k = 0; k < arrayD.length; k++) {
+                  if ( (arrayD[k][0] == i) && (arrayD[k][1] == j) ) {
+                    flag2 = false;
+                  }
+                }
+                if (flag2) {
+                  table.rows[i].cells[j].className = "active";
+                  summa += +table.rows[i].cells[j].innerText;
+                  sum.innerText = `Сумма: ${summa}`;
+                }
+
+              }
+            }
 
           } else {
 
@@ -75,7 +113,7 @@ window.onload = function () {
               }
             }
             this.className = "active";
-
+            prevElem = [cell[0],cell[1]];
           }
         }
 
